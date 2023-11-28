58aab8e4f3fd4037994bc3f05581d2330e2e052c
blake365 <68243611+blake365@users.noreply.github.com>
2023-11-27 21:41:34 -0500

    initial commit

--- theme.config.tsx
+++ theme.config.tsx
@@ -2,17 +2,17 @@ import React from 'react'
 import { DocsThemeConfig } from 'nextra-theme-docs'
 
 const config: DocsThemeConfig = {
-  logo: <span>My Project</span>,
-  project: {
-    link: 'https://github.com/shuding/nextra-docs-template',
-  },
-  chat: {
-    link: 'https://discord.com',
-  },
-  docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
-  footer: {
-    text: 'Nextra Docs Template',
-  },
+	logo: <span>NeoPromisance</span>,
+	project: {
+		link: 'https://github.com/blake365',
+	},
+	chat: {
+		link: 'https://discord.gg/bnuVy2pdgM',
+	},
+	docsRepositoryBase: 'https://github.com/blake365/neo-guide',
+	footer: {
+		text: 'NeoPromisance © 2023',
+	},
 }
 
 export default config
