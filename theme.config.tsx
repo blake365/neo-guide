fac88d2088b461b3d2272de2fb934973b5836ebb
blake365 <68243611+blake365@users.noreply.github.com>
2023-11-29 12:41:29 -0500

    strategy pages

--- theme.config.tsx
+++ theme.config.tsx
@@ -2,16 +2,16 @@ import React from 'react'
 import { DocsThemeConfig } from 'nextra-theme-docs'
 
 const config: DocsThemeConfig = {
-	logo: <span>NeoPromisance</span>,
+	logo: <strong style={{ fontSize: '2rem' }}>NeoPromisance</strong>,
 	project: {
 		link: 'https://github.com/blake365',
 	},
 	chat: {
 		link: 'https://discord.gg/bnuVy2pdgM',
 	},
-	docsRepositoryBase: 'https://github.com/blake365/neo-guide',
+	docsRepositoryBase: 'https://github.com/blake365/neo-guide/tree/main/',
 	footer: {
-		text: 'NeoPromisance © 2023',
+		text: 'NeoPromisance - 2023',
 	},
 }
 
